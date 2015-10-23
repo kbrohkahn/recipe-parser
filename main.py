af376f579bac93235acf86362c2c16a5771dfab3
Kevin Broh-Kahn <kbrohkahn@yahoo.com>
2015-10-23 12:31:50 -0400

    Convert amount to float value. Add dividing adjectives. Clean up file width.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import urllib2
 import json
 import re
-from nltk.tokenize import sent_tokenize
 from bs4 import BeautifulSoup
+from nltk.tokenize import sent_tokenize
+from socket import error as SocketError
 
 # list of measurement units, prefix (space), and suffixes (space and plurals) for parsing ingredient
-measurementUnits = [ "teaspoon", "tablespoon", "cup", "package", "can", "jar", "clove", "pinch", "pound", "container", "ounces", "square", "cake", "loave", "bar", "envelope", "pint", "quart" ]
+measurementUnits = [ "teaspoon", "tablespoon", "cup", "package", "can", "jar", "clove", "pinch", "pound", 
+		"container", "ounce", "square", "cake", "bar", "envelope", "pint", "quart", "bottle", "gallon", 
+		"bunch", "dash", "drop", "fillet", "head", "sprig", "stalk", "leaf", "leaves", "loaf", "loaves", "slice" ]
 measurementSuffixes = [ " ", "s ", "es "]
 
 # list of adjectives and adverbs used to describe ingredients
-ingredientAdverbs = [ "very ", "coarsely ", "finely ", "thinly ", "lightly ", "freshly ", "stiffly ", "well ", "well-", "super ", ""]
-ingredientAdjectives = [ "lukewarm", "warmed", "warm", "hot", "cooled", "cool", "cold", "chopped", "diced", "divided", "sliced", "thawed", "drained", "shredded", "mashed", "melted", "softened", "boiling", "dried", "beaten", "ground", "grated", "chilled", "fresh", "frozen", "cubed", "toasted", "crushed", "large", "small", "ripe", "minced", "dry", "peeled", "cored", "seeded", "deseeded", "rinsed", "sifted", "pitted", "separated", "halved", "quartered", "stemmed", "packed", "cooked", "strained", "strong", "weak", "pureed", "creamed", "fine", "or to taste", "to cover", "at room temperature", "room temperature", "with juice reserved", "juice reserved", "or as needed", "halved lengthwise", "with juice"]
+ingredientAdverbs = [ "very ", "coarsely ", "finely ", "thinly ", "lightly ", "freshly ", "stiffly ", "well ", 
+		"well-", "un", "super ", "roughly ", ""]
+ingredientAdjectives = [ "halved", "crumbled", "diced", "scalded", "chopped", "ground", "crushed", "refrigerated",
+		 "roasted", "lukewarm", "warmed", "light", "heavy", "warm", "hot", "cooled", "cool", "cold", "thawed", 
+		 "drained", "shredded", "mashed", "melted", "boiling", "dried", "beaten", "grated", "chilled", "fresh", 
+		 "frozen", "cubed", "toasted", "lean", "jumbo", "large", "small", "ripe", "minced", "dry", "peeled", 
+		 "cored", "seeded", "deseeded", "rinsed", "sifted", "pitted", "quartered", "stemmed", "packed", "cooked", 
+		 "strained", "strong", "weak", "pureed", "creamed", "fine", "firm", "slivered", "blanched", "juiced", 
+		 "washed", "trimmed", "soaked overnight", "to cover", "at room temperature", "room temperature", 
+		 "with juice reserved", "juice reserved", "or as needed", "with juice", 
+		 "deboned", "boned", "hardened", "hard", "softened", "soft"]
+
+dividingPrepBeginnings = [ "divided", "separated", "split", "sliced", "cut", "torn", "broken" ]
+dividingPrepEndings = [ "diagonally", "lengthwise", "strips", "pieces", "crumbs", "chunks", "cubes", 
+		"rounds", "slices", "cubes", "lengths", "rings", "halves", "thirds", "fourths", "eighths", 
+		"florets", "parts", "squares", "triangles" ]
 
 def main():
 	jsonFile = open("recipes.json", "w+")
@@ -38,7 +55,23 @@ def main():
 			count = len(ingredientObjects) - 3
 			ingredients = [None] * count
 			for i in range(0, count):
-				ingredient = parseIngredient(ingredientObjects[i].text)
+				ingredientDescriptions = []
+
+				ingredientString = ingredientObjects[i].text
+
+				# move parentheses to description
+				parentheses = parenthesesRegex.search(ingredientString)
+				if parentheses:
+					searchString = parentheses.group()
+					ingredientString = ingredientString.replace(searchString, "")
+					ingredientDescriptions.append(searchString)
+
+
+				# replace additional fractions with decimals
+				ingredientString = ingredientString.replace(" 1/2", ".5")
+				ingredientString = ingredientString.replace(" 1/4", ".25")
+				ingredientString = ingredientString.replace(" 1/8", ".125")
+				ingredient = parseIngredient(ingredientString)
 
 				if (ingredient == None):
 					continue
@@ -53,58 +86,70 @@ def main():
 				if " (optional)" in ingredientName:
 					ingredientName = ingredientName.replace(" (optional)", "")
 					ingredient["optional"] = True
+				elif " or to taste" in ingredientName:
+					ingredientName = ingredientName.replace(" or to taste", "")
+					ingredient["optional"] = True
 				elif " to taste" in ingredientName:
 					ingredientName = ingredientName.replace(" to taste", "")
 					ingredient["optional"] = True
 				else:
 					ingredient["optional"] = False
 
+
+				# remove dividing adjectives
+				for dividingPrepBeginning in dividingPrepBeginnings:
+					startIndex = ingredientName.find(dividingPrepBeginning)
+					if startIndex > -1:
+						for dividingPrepEnding in dividingPrepEndings:
+							endIndex = ingredientName.find(dividingPrepEnding)
+							if endIndex > -1:
+								searchString = ingredientName[startIndex:endIndex+len(dividingPrepEnding)]
+								ingredientName = ingredientName.replace(searchString, "")
+								ingredientDescriptions.append(searchString)
+								break
+
+						# adjective not followed by preposition, used as single adjective
+						if dividingPrepBeginning in ingredientName:
+							ingredientName = ingredientName.replace(dividingPrepBeginning, "")
+							ingredientDescriptions.append(dividingPrepBeginning)
+
 				# remove ingredient adjectives
-				ingredientDescription = ""
 				for ingredientAdjective in ingredientAdjectives:
 					if ingredientAdjective in ingredientName:
 						for ingredientAdverb in ingredientAdverbs:
 							searchString = ingredientAdverb + ingredientAdjective
 							if searchString in ingredientName:
-								ingredientDescription += searchString + ", "
+								ingredientDescriptions.append(searchString)
 								ingredientName = ingredientName.replace(searchString, "")
 								break
 
 				# remove ", whipped"
 				# here because "cream, whipped" different from "whipped cream"
 				if ", whipped" in ingredientName:
-					ingredientDescription += "whipped,"
-					ingredientName.replace(", whipped", "")
-
-				ingredientName.replace("semisweet", "semi-sweet")
-				ingredientName.replace(" flavored", "-flavored")
-				ingredientName.replace(" coated", "-coated")
-
-				# move regular expressions to description
-				parentheses = parenthesesRegex.search(ingredientName)
-				if parentheses:
-					searchString = parentheses.group()
-					ingredientName = ingredientName.replace(searchString, "")
-					ingredientDescription += searchString[1:] + ", "
-
-				index = ingredientName.find(" cut in")
-				if index > -1:
-					ingredientDescription += ingredientName[index+1:] + ", "
-					ingredientName = ingredientName[index:]
+					ingredientDescriptions.append("whipped")
+					ingredientName = ingredientName.replace(", whipped", "")
 
 				index = ingredientName.find(" for ")
 				if index > -1:
-					ingredientDescription += ingredientName[index+1:] + ", "
-					ingredientName = ingredientName[index:]
+					ingredientDescriptions.append(ingredientName[index+1:])
+					ingredientName = ingredientName[:index]
 
-				# remove trailing ", " from ingredient description
-				if len(ingredientDescription) > 1:
-					ingredientDescription = ingredientDescription[:-2]
-				ingredient["description"] = ingredientDescription
+				# standardize styling
+				ingredientName = ingredientName.replace(" coated", "-coated")
+				ingredientName = ingredientName.replace("fatfree", "fat-free")
+				ingredientName = ingredientName.replace("reduced ", "reduced-")
+				ingredientName = ingredientName.replace("fatfree", "fat-free")
+				ingredientName = ingredientName.replace("lowfat", "low-fat")
+				ingredientName = ingredientName.replace("low fat", "low-fat")
+				ingredientName = ingredientName.replace("semisweet", "semi-sweet")
+				ingredientName = ingredientName.replace(" flavored", "-flavored")
 
-				# clean up ingredient name
-				ingredientName = ingredientName.replace("and", "").replace(",", "").replace("  ", " ").replace(" - ", "").strip()
-				ingredient["ingredient"] = ingredientName
+				# clean up ingredient name				
+				ingredientName = ingredientName.replace(" and ", "")
+				ingredientName = ingredientName.replace(", ", "")
+				ingredientName = ingredientName.replace(" - ", "")
+				ingredientName = ingredientName.replace("  ", " ")
+				ingredientName = ingredientName.strip()
 
 				# check if singular noun (without last letter "s") is in list of all ingredients, if so remove it
 				if ingredientName[:-1] in allIngredients:
@@ -113,6 +158,8 @@ def main():
 				if ingredientName + "s" not in allIngredients:
 					allIngredients.add(ingredientName.lower())
 
+				ingredient["ingredient"] = ingredientName
+				ingredient["description"] = ingredientDescriptions
 				ingredients[i] = ingredient
 
 			# 1 empty span at end
@@ -121,7 +168,8 @@ def main():
 			for i in range(1, count):
 				directionsString += " " + directionObjects[i].text
 
-			jsonFile.write(json.dumps({"id": recipeId, "name": title, "ingredients": ingredients, "directions": sent_tokenize(directionsString)},sort_keys=True,indent=4, separators=(',', ': ')))
+			jsonFile.write(json.dumps({"id": recipeId, "name": title, "ingredients": ingredients, 
+					"directions": sent_tokenize(directionsString)},sort_keys=True,indent=4, separators=(',', ': ')))
 
 			if recipeId%10 == 0:
 				ingredientsFile = open("ingredients.txt", "w+")
@@ -138,37 +186,60 @@ def main():
 
 		except urllib2.HTTPError as e:
 			print "No recipe with id={}".format(recipeId)
+		except SocketError as e:
+			print "Socket error"
 
 	jsonFile.close()
 
-def parseIngredient(textIngredient):
-	# find first occurring measurement unit, then split text into array containing "ingredient", "amount", and "unit"
-	# ie "1 1/2 tablespoons white sugar" -> ["white sugar", "1 1/2", "tablespoons"]
-	for measurementUnit in measurementUnits:
-		for suffix in measurementSuffixes:
-			ingredient = textIngredient.split(" " + measurementUnit + suffix)
 
-			if len(ingredient) > 1:
-				return {"ingredient": ingredient[1], "amount": ingredient[0], "unit": measurementUnit}
+
+def parseIngredient(ingredient):
+	# find first occurring measurement unit, then split text into array containing "ingredient", "amount", and "unit"
+	# ie "1 tablespoon white sugar" -> ["white sugar", "1", "tablespoon"]
+	for measurementUnit in measurementUnits:
+		if measurementUnit in ingredient:
+			for suffix in measurementSuffixes:
+				index = ingredient.find(measurementUnit + suffix)
+				if index > -1:
+
+					return {"ingredient": ingredient[index+len(measurementUnit + suffix):],
+							"amount": getFloatValue(ingredient[:index]),
+							"unit": measurementUnit}
 
 	# check if not ingredient, but separator
 	# ie "For Bread:"
-	if textIngredient.find("For ") == 0 or (len(textIngredient) > 0 and textIngredient[-1:] == ":"):
+	if ingredient.find("For ") == 0 or (len(ingredient) > 0 and ingredient[-1:] == ":"):
 		return None
 
-	if " to taste" in textIngredient:
-		return {"ingredient": textIngredient, "amount": 0, "unit": "to taste" }
+	if " to taste" in ingredient:
+		return {"ingredient": ingredient, "amount": 0, "unit": "to taste" }
 
 	# no measurement unit found, "unit" is count
 	# ie "1 egg" -> ["egg", "1", "count"]
-	ingredient = textIngredient.split(" ", 1)
+	ingredient = ingredient.split(" ", 1)
 	try:
-		return {"ingredient": ingredient[1], "amount": ingredient[0], "unit": "count" }
+		return {"ingredient": ingredient[1],
+				"amount": getFloatValue(ingredient[0]),
+				"unit": "count" }
 	except IndexError as e:
 		print "\tINDEX ERROR"
-		print textIngredient
+		print ingredient
 		return None
 	
+
+
+def getFloatValue(string):
+	string = string.strip()
+	string = string.replace(" ", "+")
+	string = string.replace("/", ".0/")
+
+	try:
+		return eval(string)
+	except ValueError as e:
+		print "\tVALUE ERROR"
+		print string
+		return string
+
 
 
 # arrays for labeling ingredients (categorized for the purpose of cooking, to tomato is veg, not fruit)
@@ -180,7 +251,9 @@ meats = [ "pepperoni", "pork", "sausage", "turkey", "chicken" ]
 fishes = [ "salmon" ] 
 nutIngredients = [ "almond extract", "almonds", "walnuts", "peanuts" ]
 alcoholicIngredients = [ "beer", "wine", "rum", "vodka", "white wine", "red wine", "bourbon" ]
-spices = [ "basil", "black pepper", "red pepper", "red pepper flakes", "anise", "caraway", "cardamom", "cassava", "cayenne", "cinnamon", "fennel", "flax", "garlic", "ginger", "mace", "nutmeg", "oregano", "poppy", "rhubarb", "salt", "chocolate", "sesame", "sunflower", "thyme", "cocoa", "vanilla" ]
+spices = [ "basil", "black pepper", "red pepper", "red pepper flakes", "anise", "caraway", "cardamom", 
+		"cassava", "cayenne", "cinnamon", "fennel", "flax", "garlic", "ginger", "mace", "nutmeg", "oregano",
+		"poppy", "rhubarb", "salt", "chocolate", "sesame", "sunflower", "thyme", "cocoa", "vanilla" ]
 
 def getLabels(ingredient):
 	labels = []
