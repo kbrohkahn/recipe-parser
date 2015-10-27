3689c6ba4854d5c79809ea632641fd700f8487d9
Kevin Broh-Kahn <kbrohkahn@yahoo.com>
2015-10-27 12:09:13 -0400

    Remove prepositions earlier, improve hyphen replacing, and don't change anything to lowercase. Add footnotes, if available, and move '*' to description.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import urllib2
 import json
 import re
@@ -5,38 +6,48 @@ from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize
 from socket import error as SocketError
 
-optionalStrings = ['(optional)', 'or to taste', 'or more as needed', 'or as needed', 'as needed']
-
-# list of measurement units, prefix (space), and suffixes (space and plurals) for parsing ingredient
+# list of measurement units for parsing ingredient
 measurementUnits = ['teaspoon', 'tablespoon', 'cup', 'container', 'packet', 'bag', 'quart', 'pound',
 		'can', 'bottle', 'pint', 'package', 'fluid ounce', 'ounce', 'jar', 'head', 'gallon', 'drop', 'envelope', 'bar',
 		'box', 'pinch', 'dash', 'bunch', 'recipe', 'clove', 'layer', 'slice', 'roll', 'link', 'bulb',
-		'stalk', 'square', 'sprig', 'fillet', 'cube', 'granule', 'shell', 'leaf', 'leaves', 'loaf', 'loaves']
+		'stalk', 'square', 'sprig', 'fillet', 'piece', 'leg', 'thigh', 'cube', 'granule', 'shell', 'strip',
+		'leaf', 'leaves', 'loaf', 'loaves', 'half', 'halves']
 
-# list of adjectives and adverbs used to describe ingredients
-adverbs = ['well', 'very', 'super']
-prepositions = ['in', 'into']
-#descriptionPrefixes = ['un', 'de', 'well-']
-#descriptionSuffixes = ['ly', 'less', 'ened', 'ed', '']
-descriptions = ['baked', 'beaten', 'blanched', 'boiled', 'boiling', 'boned', 'breaded', 'brewed', 'chilled',
-		'chopped', 'cleaned', 'cooked', 'cooled', 'cored', 'creamed', 'crumbled', 'cubed', 'deboned',
-		'deseeded', 'diced', 'drained', 'dried', 'grated', 'grilled', 'halved', 'hardened', 'heated',
-		'juiced', 'julienned', 'marinated', 'mashed', 'melted', 'minced', 'opened', 'packed', 'peeled',
-		'pitted', 'popped', 'pounded', 'prepared', 'pureed', 'quartered', 'refrigerated', 'rinsed', 'roasted',
-		'roasted', 'rolled', 'scalded', 'scrubbed', 'seasoned', 'seeded', 'segmented', 'shredded', 'sifted',
-		'slivered', 'soaked', 'softened', 'stemmed', 'stewed', 'strained', 'thawed', 'tied', 'toasted',
-		'trimmed', 'unwrapped', 'vained', 'washed', 'broken', 'crushed', 'cut', 'divided', 'separated',
-		'sliced', 'split', 'torn', 'slight', 'near', 'weak',  'warm', 'thin', 'thick', 'strong', 'stiff',
-		'soft', 'small', 'skinless',
-		'rough', 'ripe', 'medium', 'lukewarm', 'light', 'lean', 'large', 'jumbo', 'hot', 'heavy', 'hard', 'ground', 'frozen',
-		'fresh', 'firm', 'fine', 'dry', 'crisp', 'cool', 'cold', 'coarse']
-nonFirstWordDescriptions = ['creamed', 'whole', 'whipped']
-descriptionsWithPredecessor = ['removed', 'reserved']
-descriptionsWithSuccessor = ['for', 'with']
+# list of length units used to ignore preceding digit, ie "1 12 inch pan""
+lengthUnits = ['inch']
 
-#otherDescriptions = ['soaked overnight', 'to cover', 'at room temperature', 'room temperature']
+# list of adverbs used before or after description
+adverbs = ['well', 'very', 'super', 'diagonally', 'lengthwise', 'overnight']
 
+# list of adjectives and participles used to describe ingredients
+descriptions = ['baked', 'beaten', 'blanched', 'boiled', 'boiling', 'boned', 'breaded', 'brewed', 'broken', 'chilled',
+		'chopped', 'cleaned', 'coarse', 'cold', 'cooked', 'cool', 'cooled', 'cored', 'creamed', 'crisp', 'crumbled',
+		'crushed', 'cubed', 'cut', 'deboned', 'deseeded', 'diced', 'dissolved', 'divided', 'drained', 'dried', 'dry',
+		'fine', 'firm', 'fluid', 'fresh', 'frozen', 'grated', 'grilled', 'ground', 'halved', 'hard', 'hardened',
+		'heated', 'heavy', 'hot', 'juiced', 'julienned', 'jumbo', 'large', 'lean', 'light', 'lukewarm', 'marinated',
+		'mashed', 'medium', 'melted', 'minced', 'near', 'opened', 'optional', 'packed', 'peeled', 'pitted', 'popped',
+		'pounded', 'prepared', 'pressed', 'pureed', 'quartered', 'refrigerated', 'rinsed', 'ripe', 'roasted',
+		'roasted', 'rolled', 'rough', 'scalded', 'scrubbed', 'seasoned', 'seeded', 'segmented', 'separated',
+		'shredded', 'sifted', 'skinless', 'sliced', 'slight', 'slivered', 'small', 'soaked', 'soft', 'softened',
+		'split', 'squeezed', 'stemmed', 'stewed', 'stiff', 'strained', 'strong', 'thawed', 'thick', 'thin', 'tied', 
+		'toasted', 'torn', 'trimmed', 'unwrapped', 'vained', 'warm', 'washed', 'weak', 'zested', 'wedged',
+		'skinned', 'gutted', 'browned']
 
+# list of prepositions used after ingredient name
+prepositions = ['as', 'such', 'to', 'for', 'with', 'without', 'if', 'about']
+
+# list of prepositions used after ingredient name and immediately following adjective / participle
+prepositionsWithPrecedingAdjective = ['in', 'into', 'at']
+
+# list of prefixes and suffixes that should be hyphenated
+hypenatedPrefixes = ['non', 'reduced', 'semi', 'low']
+hypenatedSuffixes = ['coated', 'free', 'flavored']
+
+# only used as <something> removed, <something> reserved, <x> inches, <x> old, <some> temperature
+descriptionsWithPredecessor = ['removed', 'reserved', 'inch', 'inches', 'old', 'temperature']
+
+# strings indicating ingredient as optional
+optionalStrings = ['optional', 'to taste', 'as needed', 'if desired']
 
 # arrays for labeling ingredients (categorized for the purpose of cooking, to tomato is veg, not fruit)
 nonDairyMilk = [ "almond milk", "soy milk", "coconut milk" ]
@@ -76,6 +87,7 @@ def main():
 			directionObjects = soup.find_all("span", class_="recipe-directions__list--item")
 			servingSpan = soup.find("span", class_="servings-count")
 			calorieSpan = soup.find("span", class_="calorie-count")
+			footnotesSection = soup.find("section", class_="recipe-footnotes")
 
 			# ingredients
 			# 2 spans with "Add all" and 1 empty, always last 3 spans
@@ -86,18 +98,11 @@ def main():
 				
 				# check if not ingredient, but separator
 				# ie "For Bread:"
-				if ingredientString.find("For ") == 0 or ingredientString[-1:] == ":" or " " not in ingredientString:
+				if ingredientString.find("For ") == 0 or " " not in ingredientString or \
+						(":" in ingredientString and "eg:" not in ingredientString):
 					continue
 				
 				ingredient = {}
-
-				# get whether optional
-				ingredient["optional"] = False
-				for optionalString in optionalStrings:
-					if optionalString in ingredientString:
-						ingredientString = ingredientString.replace(optionalString, "")
-						ingredient["optional"] = True
-						break
 
 				ingredient["descriptions"] = []
 
@@ -110,22 +115,39 @@ def main():
 					ingredientString = ingredientString.replace(searchString, "")
 					ingredient["descriptions"].append(searchString[1:-1])
 
-				# split ingredient into words, removing "," and "-"
-				ingredientString = ingredientString.replace(",","")
+				# remove "," and "-" then split ingredient into words
+				ingredientString = ingredientString.replace(","," ")
 				ingredientString = ingredientString.replace("-"," ")
 				parsedIngredient = ingredientString.split(" ")
 
-				# remove "", caused by extra spaces, from parsed ingredient
+				# remove "", caused by extra spaces
 				while "" in parsedIngredient:
 					parsedIngredient.remove("")
 				
-				# remove "and" from parsed ingredient
+				# remove "and"
 				while "and" in parsedIngredient:
 					parsedIngredient.remove("and")
 				
-				# remove "style" from parsed ingredient
+				# remove "style"
 				while "style" in parsedIngredient:
 					parsedIngredient.remove("style")
+
+				# move prepositions to description
+				splitIndex=-1
+				for index in range(0, len(parsedIngredient)):
+					word = parsedIngredient[index]
+					if word in prepositionsWithPrecedingAdjective:
+						splitIndex = index - 1
+					elif word in prepositions:
+						splitIndex = index
+
+					if splitIndex > -1:
+						parsedPrepositionalPhrase = parsedIngredient[splitIndex:]
+						ingredient["descriptions"].append(" ".join(parsedPrepositionalPhrase))
+						parsedIngredient = parsedIngredient[:splitIndex]
+						break
+
+
 
 				#
 				# get amount
@@ -136,7 +158,8 @@ def main():
 					firstWord = parsedIngredient[0]
 
 					# first letter not a digit, so amountString is complete
-					if not firstWord[0].isdigit():
+					# or next word is length unit, ie "1 12x12 INCH pan"
+					if not firstWord[0].isdigit() or len(parsedIngredient) > 1 and parsedIngredient[1] in lengthUnits:
 						break
 
 					# move first word to amountString
@@ -161,103 +184,115 @@ def main():
 
 				ingredient["unit"] = unitString
 
+
+				if len(parsedIngredient) > 0 and parsedIngredient[0] == "of":
+					# delete "of" at first index, ie "1 cup of milk" -> "1 cup milk"
+					del parsedIngredient[0]
+				
 				#
 				# get descriptions
 				#
 				index = 0
-				descriptionString = ""
+				descriptionString = " "
 				while index < len(parsedIngredient):
 					wordToDescription = False
 					descriptionPhraseComplete = True
 					word = parsedIngredient[index]
+
+					# search through descriptions (adjectives)
 					for description in descriptions:
 						if description in word:
 							wordToDescription = True
 							break
 
-					if not wordToDescription and index > 1 and word in nonFirstWordDescriptions:
-						wordToDescription = True
+					# search through hyphenated suffixes, ie "fatfree"
+					for hypenatedSuffix in hypenatedSuffixes:
+						if hypenatedSuffix in word:
+							word.replace(hypenatedSuffix, "-" + hypenatedSuffix)
+					
+					# search through hyphenated suffixes, ie "lowfat"
+					for hypenatedPrefix in hypenatedPrefixes:
+						if word.find(hypenatedPrefix) == 0:
+							word.replace(hypenatedPrefix, hypenatedPrefix + "-")
 
+					# adverb, wait for following adjective
 					if not wordToDescription and word in adverbs:
 						wordToDescription = True
 						descriptionPhraseComplete = False
 
-					if not wordToDescription and word in descriptionsWithSuccessor and index + 1 < len(parsedIngredient):
-						# word followed immediately by successor, ie "with pudding"
-						wordToDescription = True
-						del parsedIngredient[index]
-						if descriptionString != "":
-							descriptionString += " "
-						descriptionString += word
-
-						word=parsedIngredient[index]
-
+					# word followed immediately by successor, ie "pudding reserved"
 					if not wordToDescription and word in descriptionsWithPredecessor and index > 1:
-						# word followed immediately by successor, ie "pudding reserved"
 						index-=1
 						word=parsedIngredient[index]
 
 						wordToDescription = True
-						del parsedIngredient[index]
-						if descriptionString != "":
-							descriptionString += " "
-						descriptionString += word
 						
+						descriptionString += " " + word
+						del parsedIngredient[index]
 						word=parsedIngredient[index]
 
-					if wordToDescription and index + 1 < len(parsedIngredient) and parsedIngredient[index + 1] in prepositions:
-						# adjective followed by preposition
-						while index + 1 < len(parsedIngredient):
-							if descriptionString != "":
-								descriptionString += " "
-							descriptionString += word
-	
-							del parsedIngredient[index]
-							word = parsedIngredient[index]
+					# word is not always description, sometimes part of ingredient
+					if not wordToDescription and ((word == "whole" and "wheat" not in parsedIngredient) or \
+							(word == "creamed" and "cheese" not in parsedIngredient) or \
+							(word == "whipped" and "cream" not in parsedIngredient and "topping" not in parsedIngredient)):
+						wordToDescription = True
 
-					if wordToDescription and len(descriptionString) > 2 and descriptionString[-2:] == "ly":
-						# adjective ends in "ly", so used as adverb
+					# next word is adverb
+					if wordToDescription and index + 1 < len(parsedIngredient) and parsedIngredient[index + 1] in adverbs:
 						descriptionPhraseComplete = False
 
+					# adjective ends in "ly", so used as adverb
+					if wordToDescription and len(descriptionString) > 2 and descriptionString[-2:] == "ly":
+						descriptionPhraseComplete = False
+
+					# append word to description string
 					if wordToDescription:
+						descriptionString += " " + word
 						del parsedIngredient[index]
-						if descriptionString != "":
-							descriptionString += " "
-						descriptionString += word
-
+						
+						# description phrase complete, add to list
 						if descriptionPhraseComplete:
-							ingredient["descriptions"].append(descriptionString)
-							descriptionString = ""
+							ingredient["descriptions"].append(descriptionString[1:])
+							descriptionString = " "
 
+					# word part of ingredient, set modified word and increment index
 					else:
+						parsedIngredient[index] = word
 						index+=1
+
+				if len(descriptionString) > 1:
+					ingredient["descriptions"].append(descriptionString[1:])
+
+				if len(parsedIngredient) > 1 and parsedIngredient[len(parsedIngredient) - 1] == "or":
+					del parsedIngredient[len(parsedIngredient) - 1]
 
 				#
 				# get ingredient
 				#
 				ingredientString = " ".join(parsedIngredient)
 
-				# standardize "-" styling
-				ingredientString = ingredientString.replace(" coated", "-coated")
-				ingredientString = ingredientString.replace(" free", "-free")
-				ingredientString = ingredientString.replace("fatfree", "fat-free")
-				ingredientString = ingredientString.replace("reduced ", "reduced-")
-				ingredientString = ingredientString.replace("lowfat", "low-fat")
-				ingredientString = ingredientString.replace("low fat", "low-fat")
-				ingredientString = ingredientString.replace("semi ", "semi-")
-				ingredientString = ingredientString.replace("semisweet", "semi-sweet")
-				ingredientString = ingredientString.replace(" flavored", "-flavored")
-				ingredientString = ingredientString.replace("all purpose", "all-purpose")
-				ingredientString = ingredientString.replace("jell o", "jell-o")
+				# remove "*", add footnote to description
+				if "*" in ingredientString:
+					ingredient["descriptions"].append("* see footnote")
+					ingredientString.replace("*", "")
 
-				# check if singular noun (without last letter "s") is in list of all ingredients, if so remove it
+				# standardize "-" styling and fix spelling errors
+				ingredientString = ingredientString.replace("- ", "-")
+				ingredientString = ingredientString.replace(" -", "-")
+				ingredientString = ingredientString.replace("Jell O", "Jell-O")
+				ingredientString = ingredientString.replace("half half", "half-and-half")
+				ingredientString = ingredientString.replace("linguini", "linguine")
+
+				# if singular noun (without last letter "s") is in list of all ingredients, remove it
 				if ingredientString[:-1] in allIngredients:
 					allIngredients.remove(ingredientString[:-1])
-				# add ingredient name to list of all ingredients
+				
+				# if plural of ingredientString is in list, make string plural
 				if ingredientString + "s" in allIngredients:
 					ingredientString += "s"
+				# else add ingredient name to list of all ingredients
 				else:
-					allIngredients.add(ingredientString.lower())
+					allIngredients.add(ingredientString)
 
 				ingredient["ingredient"] = ingredientString
 
@@ -280,6 +315,15 @@ def main():
 				if ingredient in alcoholicIngredients:
 					labels.append("alcohol")
 				ingredient["labels"] = labels
+
+				#
+				# get whether optional
+				#
+				ingredient["optional"] = False
+				for optionalString in optionalStrings:
+					if optionalString in ingredient["descriptions"]:
+						ingredient["optional"] = True
+						break
 				
 				ingredients.append(ingredient)
 
@@ -293,10 +337,16 @@ def main():
 			for i in range(1, count):
 				directionsString += " " + directionObjects[i].text
 
+			footnotes = []
+			if footnotesSection:
+				for footnote in footnotesSection.find_all("li"):
+					footnotes.append(footnote.text)
+
 			jsonFile.write(json.dumps({"id": recipeId,
 										"name": title,
 										"ingredients": ingredients, 
 										"directions": sent_tokenize(directionsString),
+										"footnotes": footnotes,
 										"servings": (servingSpan.contents[0].text if servingSpan else ""),
 										"calories": (calorieSpan.contents[0].text if calorieSpan else "")},
 					sort_keys=True,
@@ -322,21 +372,23 @@ def main():
 
 
 def getUnitString(parsedIngredient):
-	unitString = parsedIngredient[0]
-	for measurementUnit in measurementUnits:
-		if unitString.find(measurementUnit) == 0:
-			return unitString
-
-	# check for "cake" as unit, but only if "yeast" somewhere in ingredient
-	for word in parsedIngredient:
-		if "yeast" in word:
-			for word in parsedIngredient:
-				if word.find("cake") == 0:
-					return word
-
+	# check words for unit
 	for word in parsedIngredient:
 		for measurementUnit in measurementUnits:
-			if word.find(measurementUnit) == 0:
+			if equalIncludingPlurals(word, measurementUnit):
+				return word
+
+	# check for "cake" as unit, but only if "yeast" somewhere in ingredient
+	if "yeast" in parsedIngredient:
+		for word in parsedIngredient:
+			if equalIncludingPlurals(word, "cake"):
 				return word
 
 	return None
+
+
+
+def equalIncludingPlurals(string, singularString):
+	return string == singularString or string == singularString + "s" or string == singularString + "es"
+
+
